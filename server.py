3681306f5a1d470cbfd351e90a24d0295cf4c59a
gaga5lala <gaga5lala1013@gmail.com>
2023-05-15 21:33:08 +0800

    read files

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 import subprocess
 import os
+from flask import Flask, send_from_directory
+from flask_cors import CORS
 
 app = Flask(__name__)
 work_dir = os.path.expanduser('~')  # initial working directory
@@ -13,6 +15,20 @@ with open('blacklist.txt', 'r') as f:
 def is_blacklisted(command):
     return any(blacklisted in command.split() for blacklisted in blacklist)
 
+
+cors = CORS(app, resources={r"/*": {"origins": "https://chat.openai.com"}})
+
+@app.route('/openapi.yaml')
+def openapi_yaml():
+    return send_from_directory(app.static_folder, 'openapi.yaml')
+
+@app.route('/logo.png')
+def logo_png():
+    return send_from_directory(app.static_folder, 'logo.png')
+
+@app.route('/.well-known/ai-plugin.json')
+def manifest_json():
+    return send_from_directory(app.static_folder, '.well-known/ai-plugin.json')
 
 @app.route('/execute', methods=['POST'])
 def execute():
